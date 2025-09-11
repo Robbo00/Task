8e69c958a7a553071310febb808525147312c328
Robbo00 <rscrog606@west-mec.org>
2025-09-11 15:24:35 -0700

    .

--- task.js
+++ task.js
@@ -2,6 +2,8 @@ let tasks = []
 let completed = []
 let header = document.getElementById('header')
 let curr = 'All'
+let add = ' <input id="tHead" type="text" placeholder="Task"> <input id="tRemind type="text" placeholder="Reminder"> <input id="tDate" type="date"> <button onclick="app()">Add Task</button> '
+let display = document.getElementById('disp')
 
 try{
     tasks = localStorage.getItem('tasks')
@@ -12,7 +14,8 @@ catch{
 }
 
 function change(title){
-    document.getElementById(curr).style.borderBottom = 'none'
+        clear()
+        document.getElementById(curr).style.borderBottom = 'none'
     if(title == 'All Tasks'){
             header.innerHTML = 'All Tasks'
             curr = 'All'
@@ -29,8 +32,27 @@ function change(title){
     }
 
     if(title == 'Create'){
+        let fresh = document.createElement('div')
+        fresh.setAttribute('id', 'task')
+        fresh.setAttribute('class', 'task')
+b        display.appendChild(fresh)
             header.innerHTML = 'Add a Task'
             curr = 'Create'
     }
+
     document.getElementById(curr).style.borderBottom ='solid .3vh white'
+}
+
+function clear(){
+        
+    for (let i = 0; i < display.children.length; i++) {
+        if(display.children[i].localName == 'div'){
+                console.log('ran')
+                display.children[i].outerHTML = ''
+        }
+    }
+}
+
+function app(){
+
 }
